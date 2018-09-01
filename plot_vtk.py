c6fb94d00332b4d141fc0610ccc9a23821f7da55
Dan <>
2018-08-31 21:50:15 -0400

    Added a lot of new features.

---
+++ plot_vtk.py
@@ -0,0 +1,27 @@
+#plot_vtk.py
+
+from read_vtk import read_vtk
+from line_in_3d import line_in_z_3d
+import matplotlib.pyplot as plt
+import os
+
+def plot_dens_1D(indir,fn,outdir):
+    inputfile = os.path.join(indir,fn)
+    print("input: %s" % inputfile)
+    tmpvtk = read_vtk(inputfile)
+    grid = tmpvtk[0]
+    dens = tmpvtk[3]
+    gx = grid[0]//1
+    gy = grid[1]//2
+    dens1D = line_in_z_3d(dens,grid,gx,gy)
+    plt.figure()
+    plt.plot(dens1D,'ro')
+    outname = fn[:-4] + ".pdf"
+    outputfile = os.path.join(outdir,outname)
+    print("output: %s" % outputfile)
+    plt.savefig(outputfile)
+
+def multi_plot_dens_1D(indir,outdir):
+    files = [f for f in os.listdir(indir) if os.path.isfile(os.path.join(indir,f))]
+    for f in files:
+        plot_dens_1D(indir,f,outdir)
--- va_main.py
+++ va_main.py
@@ -2,26 +2,42 @@
 
 from read_vtk import read_vtk
 from line_in_3d import line_in_z_3d
-import matplotlib.pyplot as plt
+from plot_vtk import plot_dens_1D, multi_plot_dens_1D
+import os, sys, getopt
 
-def va():
+def va(indir,outdir):
     print("vtk analysis")
-    fname = 'sample.vtk'
-    tmpvtk = read_vtk(fname)
-    grid = tmpvtk[0]
-    spc = tmpvtk[1]
-    org = tmpvtk[2]
-    dens = tmpvtk[3]
-    dens_1d = line_in_z_3d(dens,grid,13,13)
-    plt.plot(dens_1d,'ro')
-    plt.show()
-    print('end')
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    '''
+    files = [f for f in os.listdir(indir) if os.path.isfile(os.path.join(indir,f))]
+    plot_dens_1D(indir,files[0],outdir)
+    '''
+    multi_plot_dens_1D(indir,outdir)
+    print("end")
 
-def main():
+def main(argv):
+    inputdir = ''
+    outputdir = ''
     try:
-        va()
+        opts, args = getopt.getopt(argv,"hi:o:",["idir=","odir="])
+    except getopt.GetoptError:
+        print ("test.py -i <inputdir> -o <outputdir>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("test.py -i <inputdir> -o <outputdir>")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputdir = arg
+        elif opt in ("-o", "--ofile"):
+            outputdir = arg
+            print ("Input dir is %s" % inputdir)
+            print ("Output dir is %s" % outputdir)
+    try:
+        va(str(inputdir),str(outputdir))
     except:
         print("Failed.")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
